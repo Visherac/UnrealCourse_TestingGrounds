d6fa89bc7a000900a1fdbb875c5d4df48f93823b
Visherac <gesundheit999@gmail.com>
2019-02-13 20:44:58 -0800

    added Patrol Component so we don't rely on class inheiritance for patrol routes.

--- Source/TestingGrounds/ChooseNextPatrolPoint.cpp
+++ Source/TestingGrounds/ChooseNextPatrolPoint.cpp
@@ -3,24 +3,35 @@
 #include "ChooseNextPatrolPoint.h"
 #include "BehaviorTree/BehaviorTreeComponent.h"
 #include "BehaviorTree/BlackboardComponent.h"
-#include "public/PatrollingGuard.h"
 #include "AIController.h"
+#include "PatrolComponent.h"
 
 EBTNodeResult::Type UChooseNextPatrolPoint::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
 {
 	//get patrol points
 	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
-	auto Pawn = Cast<APatrollingGuard>(OwnerComp.GetAIOwner()->GetPawn());
-	auto PatrolPoints = Pawn->PatrolPointsCPP;
+	auto Pawn = OwnerComp.GetAIOwner()->GetPawn();
+	auto PatrolComponent = Pawn->FindComponentByClass<UPatrolComponent>();
+
+	if (!ensure(PatrolComponent)) { return EBTNodeResult::Failed; }
 	
+	auto PatrolPoints = PatrolComponent->GetPatrolPoints();
+
+	if (PatrolPoints.Num() <= 0) 
+	{ 
+		UE_LOG(LogTemp, Warning, TEXT("%s is missing a Patrol Point assignments."), *Pawn->GetName())
+		return EBTNodeResult::Failed; 
+	}
+
 	//get next control point
 	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
 	auto NextPatrolPoint = PatrolPoints[Index];
 	BlackboardComp->SetValueAsObject(PatrolPointKey.SelectedKeyName, NextPatrolPoint);
-	
+
 	//increment index
 	int32 NextIndex = (Index + 1) % PatrolPoints.Num();
 	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
-	
+
 	return EBTNodeResult::Succeeded;
+	
 }
---
+++ Source/TestingGrounds/Public/PatrolComponent.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "PatrolComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class TESTINGGROUNDS_API UPatrolComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UPatrolComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	TArray<AActor*> GetPatrolPoints() const;
+
+protected:
+	UPROPERTY(EditInstanceOnly, Category = Patrol)
+	TArray<AActor*> PatrolPoints;
+		
+	
+};
