31af05dcfc7f2a6dc14f3a6e6e4cd6f7a271542d
Visherac <gesundheit999@gmail.com>
2019-02-14 23:10:58 -0800

    gun is working after refactor.

---
+++ Source/TestingGrounds/Weapons/PracticeGun.h
@@ -0,0 +1,65 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PracticeGun.generated.h"
+
+class USkeletalMeshComponent;
+class USceneComponent;
+class UAnimInstance;
+class USoundBase;
+class ABallProjectile;
+class UAnimMontage;
+
+UCLASS()
+class TESTINGGROUNDS_API APracticeGun : public AActor
+{
+	GENERATED_BODY()
+	
+
+	/** Gun mesh: 1st person view (seen only by self) */
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+	USkeletalMeshComponent* FP_Gun;
+
+	/** Location on gun mesh where projectiles should spawn. */
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+	USceneComponent* FP_MuzzleLocation;
+
+public:	
+	// Sets default values for this actor's properties
+	APracticeGun();
+
+
+	void OnFire();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	/** Projectile class to spawn */
+	UPROPERTY(EditDefaultsOnly, Category = Projectile)
+	TSubclassOf<ABallProjectile> ProjectileClass;
+
+	/** Sound to play each time we fire */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	USoundBase* FireSound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	UAnimInstance* AnimInstance;
+
+	/** AnimMontage to play each time we fire */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	UAnimMontage* FireAnimation;
+protected:
+
+private:
+	
+	
+	
+};
